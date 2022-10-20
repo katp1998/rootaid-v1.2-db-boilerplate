216eb2f10c4fce4f9a231be4c07462ee75c37365
katp98 <kethzyp@gmail.com>
2022-10-20 12:45:30 +0530

    db-postgres-express: init
    Signed-off-by: katp98 <kethzyp@gmail.com>

---
+++ db-configurations-and-models/db-postgres-express/src/database/user.repository.ts
@@ -0,0 +1,20 @@
+import { Request, Response } from 'express';
+import { pool } from './connection';
+import { QueryResult } from 'pg';
+
+export const createUser = async (req: Request, res: Response) => {
+    const { name, email } = req.body;
+    const response = await pool.query('INSERT INTO users (name, email) VALUES ($1, $2)', [name, email]);
+    res.json({
+        message: 'User Added successfully',
+        body: {
+            user: { name, email }
+        }
+    })
+};
+
+export const getUserById = async (req: Request, res: Response): Promise<Response> => {
+    const id = parseInt(req.params.id);
+    const response: QueryResult = await pool.query('SELECT * FROM users WHERE id = $1', [id]);
+    return res.json(response.rows);
+};
