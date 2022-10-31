0b16eceea6817f11aa4e5ee41ad045b6b1b9bfa1
katp98 <kethzyp@gmail.com>
2022-10-31 10:10:03 +0530

    error fixed
    Signed-off-by: katp98 <kethzyp@gmail.com>

--- frontend-react-redux/src/features/auth/authSlice.tsx
+++ frontend-react-redux/src/features/auth/authSlice.tsx
@@ -18,7 +18,7 @@ const initialState: InitialState = {
 }
 
 // Generates pending, fulfilled and rejected action types
-export const fetchUsers = createAsyncThunk('user/fetchUsers', () => {
+export const login = createAsyncThunk('user/login', () => {
   return axios
     .get('') //add url here
     .then(response => response.data)
@@ -29,18 +29,18 @@ const userSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers: builder => {
-    builder.addCase(fetchUsers.pending, state => {
+    builder.addCase(login.pending, state => {
       state.loading = true
     })
     builder.addCase(
-      fetchUsers.fulfilled,
+        login.fulfilled,
       (state, action: PayloadAction<User[]>) => {
         state.loading = false
         state.users = action.payload
         state.error = ''
       }
     )
-    builder.addCase(fetchUsers.rejected, (state, action) => {
+    builder.addCase(login.rejected, (state, action) => {
       state.loading = false
       state.users = []
       state.error = action.error.message || 'Something went wrong'
