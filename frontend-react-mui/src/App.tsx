a5d542d284cf5be71caf75c34c85ae96262522cd
katp98 <kethzyp@gmail.com>
2022-11-01 11:43:50 +0530

    mui --ip
    Signed-off-by: katp98 <kethzyp@gmail.com>

--- frontend-react-mui/src/App.tsx
+++ frontend-react-mui/src/App.tsx
@@ -1,23 +1,32 @@
 import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
+
+//IMPORTING COMPONENTS:
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+//IMPORTING PAGES:
+import HomePage from './pages/HomePage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-                <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Header />
+      <main>
+          <Routes>
+          <Route path='/' element={<HomePage />}></Route>
+          <Route path='/login' element={<LoginPage />}></Route>
+          <Route path='/register' element={<RegisterPage />}></Route>
+          </Routes>
+      </main>
+      <Footer />
+    </Router>
+    </>
   );
 }
 
