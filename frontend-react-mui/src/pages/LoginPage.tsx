8c877aab3a2299bff79aa4d11b45baf977e928ea
katp98 <kethzyp@gmail.com>
2022-11-03 13:34:12 +0530

    MUI - login -ip
    Signed-off-by: katp98 <kethzyp@gmail.com>

--- frontend-react-mui/src/pages/LoginPage.tsx
+++ frontend-react-mui/src/pages/LoginPage.tsx
@@ -1,38 +1,40 @@
 import React from 'react'
 
 //IMPORTING MUI LIBS:
-import Box from '@mui/material/Box';
-import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
-import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
-import Typography from '@mui/material/Typography';
+import {Box, Typography, TextField, Button} from '@mui/material/';
+import { hover } from '@testing-library/user-event/dist/hover';
 
 
 
 const LoginPage = () => {
   return (
-    <>
-      <Card sx={{ minWidth: 275 }}>
-        <CardContent>
-          <Typography variant="h5" component="div">
-            LOGIN
-          </Typography>
-          <Typography sx={{ mb: 1.5 }} color="text.secondary">
-            adjective
-          </Typography>
-          <Typography variant="body2">
-            well meaning and kindly.
-            <br /> 
-            {'"a benevolent smile"'}
-          </Typography>
-        </CardContent>
-        <CardActions>
-          <Button size="small">Learn More</Button>
-        </CardActions>
-      </Card>
-    
-    </>
+    <div>
+      <form action="">
+        <Box 
+        display = "flex" 
+        flexDirection={"column"} 
+        maxWidth = {400} 
+        alignItems="center" 
+        justifyContent={"center"} 
+        margin="auto" 
+        marginTop={5} 
+        padding={'50px'} 
+        borderRadius={5} 
+        boxShadow={'5px 5px 10px #ccc'}
+        sx={{":hover":{
+          boxShadow:'10px 10px 20px #ccc'
+        }}}>
+          <Typography 
+          variant='h3'
+          padding= {3}
+          textAlign = "center">LOGIN</Typography>
+          <TextField label="Email" variant="outlined" multiline placeholder='Enter email address' margin='normal' fullWidth color='error' />
+          <TextField type="password" label="Password" variant="outlined" multiline placeholder='Enter password' margin="normal" fullWidth color='error' id="outlined-password-input" />
+          <Button variant ="contained" color="error" size="large">Login</Button>  
+        </Box>
+
+      </form>
+    </div>
   )
 }
 
